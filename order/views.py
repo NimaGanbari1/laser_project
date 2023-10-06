346bae3f155d7112a7825be528c5c181cdecdcf9
nima ghanbari <nimagithub1401@gmail.com>
2023-10-06 09:41:35 +0330

    edit 6

--- order/views.py
+++ order/views.py
@@ -18,6 +18,8 @@ from azbankgateways import bankfactories, models as bank_models, default_setting
 from .models import Order
 from Cart.models import Cart
 from products.models import Product
+from Authentication.views import send_sms
+from dj_laser.local_settings import SHOP_OWNER_NUMBER
 
 User = get_user_model()
 
@@ -44,92 +46,79 @@ def callback_gateway_view(request):
 
 # When returning from the bank, it is transferred to this function
 def Ok_Record(request):
-    # البت بهتر است که وقتی سفارشی ثبت شد یک پیام یا ایمیل برای ادمین فرستاده شود
+    # However, it is better to send a message or email to the admin when the order is registered
     # In this part, we got the information related to the creation of the object
     # we have access to them through 'products':products,'price':TotalPrice,'user':user
     if request.method == "GET":
         status = callback_gateway_view(request)
         context = None
         match status:
+            # void
             case 0:
-                print("0")
+                # If the return link is not valid
+                messages.error(request, "این لینک معتبر نیست.", "failed")
+                context = {
+                    "status": "این لینک معتبر نیست."
+                }   
+                
+            # pay
             case 10:
-                print("10")
+                # Part 1
+                products = Cart.objects.filter(user=request.user).values()
+                ProductCodes = []
+                ProductCounts = []
+                for x in products:
+                    ProductCodes.append(str(list(x.values())[1]))
+                    ProductCounts.append(str(list(x.values())[2]))
+                    
+                # Part 2
+                # Calculate the final price
+                user = str(request.user)
+                TotalPrice = 0
+                for x in products:
+                    temp = Product.objects.get(uniqe_code=list(x.values())[1])
+                    price = temp.price
+                    price *= list(x.values())[2]
+                    TotalPrice += price
+                # Part 3
+                tempuser = User.objects.get(id = request.user.id)
+                # Part 4
+                rand_code = random.randint(10000, 99999)
+                ortemp = Order.objects.create(ProductCodes=ProductCodes, ProductCounts=ProductCounts, Price=TotalPrice, user=tempuser, Address=tempuser.address,
+                                            consumed_code=rand_code, status_pay=10)
+                
+                # Part 5
+                # All items in the user's shopping cart will be deleted
+                products = Cart.objects.filter(user=request.user).delete()
+
+                # Part 6
+                # A message will be sent to the admin for a new order in this section
+                sms = send_sms(f"{SHOP_OWNER_NUMBER}",f"Hey, new order consumed_code: {rand_code}")
+                
+                # messages.success(request,"Your order has been successfully placed")
+                context = {
+                    "status": "Payment was successful"
+                }
+                
+            # error    
             case 20:
                 print("20")
+                # به هر دلیلی که پرداخت انجام نشد به این قسمت می آید
+                messages.error(
+                    request, "پرداخت با شکست مواجه شده است. اگر پول کم شده است ظرف مدت ۴۸ ساعت پول به حساب شما بازخواهد گشت.", "failed")
+                context = {
+                    "status": "Payment failed"
+                }
+                
+            # cancel    
             case 30:
                 print("30")
-            case 31:
-                print("31")
                 
-        if status == 0:
-            # If the return link is not valid
-            messages.error(request, "این لینک معتبر نیست.", "failed")
-            context = {
-                "status": "این لینک معتبر نیست."
-            }
-        # void
-        elif status == 10:
-            pass
-            # یک آبجکت میسازیم و ذخیره میکنیم و به ادمین یک پیغام میدهیم
-            # تعداد و کد محصول ها در متغیر پایینی است
-            products = Cart.objects.filter(user=request.user).values()
-            ProductCodes = []
-            ProductCounts = []
-            for x in products:
-                ProductCodes.append(str(list(x.values())[1]))
-                ProductCounts.append(str(list(x.values())[2]))
-            ###############################################
-            # قیمت نهایی محاسبه شود
-            user = str(request.user)
-            TotalPrice = 0
-            for x in products:
-                # TotalProduct.append(Product.objects.get(uniqe_code=list(x.values())[1]))
-                temp = Product.objects.get(uniqe_code=list(x.values())[1])
-                price = temp.price
-                price *= list(x.values())[2]
-                # PricePerGood.append(price)
-                TotalPrice += price
-                # tempuser = User.objects.get()
-            ##########################################################
-            # اینم برای یوزر
-            #phonemail = str(request.user)
-            #tempuser = ''
-            #if phonemail.isdigit():
-            #    tempuser = User.objects.get(phone_number=int(phonemail))
-            #else:
-            #    tempuser = User.objects.get(email=phonemail)
+            # refunded
+            case 31:
+                pass
             
-            tempuser = User.objects.get(id = request.user.id)
-            ##################################
-
-            ortemp = Order.objects.create(ProductCodes=ProductCodes, ProductCounts=ProductCounts, Price=TotalPrice, user=tempuser, Address=tempuser.address,
-                                          consumed_code=random.randint(10000, 99999), status_pay=10)
-            # حالا تمام سفارشات مربوز به
-            # cart
-            # که مربوط به این کاربر میباشد باید حذف شود
-            products = Cart.objects.filter(user=request.user).delete()
-
-            # به ادمین یک پیام جهت سفارش جدید زده میشود در این قسمت
-            # messages.success(request,"Your order has been successfully placed")
-            context = {
-                "status": "Payment was successful"
-            }
-        # pay
-        elif status == 20:
-            # به هر دلیلی که پرداخت انجام نشد به این قسمت می آید
-            messages.error(
-                request, "پرداخت با شکست مواجه شده است. اگر پول کم شده است ظرف مدت ۴۸ ساعت پول به حساب شما بازخواهد گشت.", "failed")
-            context = {
-                "status": "Payment failed"
-            }
-        # error
-        elif status == 30:
-            pass
-        # cancel
-        elif status == 31:
-            pass
-        # refunded
         return render(request, "order/final.html/", context=context)
+            
     else:
         return redirect('/')
